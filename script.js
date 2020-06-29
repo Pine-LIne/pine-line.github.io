bcecea4de02bd4458df71024d8d2d205f444b780
Dre Cardinalli <rcard@hawaii.edu>
2020-06-29 13:44:52 -1000

    basic file generation complete
    + main.html: added FileSaver.js script; added our own basic script.js
    + script.js:  implemented basic data and functionality for generating a downloadable file.

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 
 
 function submitForm() {
-
+    createFile();
 }
 
 function createFile() {
+    const data = "BEGIN:VCALENDAR\n" +
+        "PRODID:-//xyz Corp//NONSGML PDA Calendar Version 1.0//EN\n" +
+        "VERSION:2.0\n" +
+        "BEGIN:VEVENT\n" +
+        "DTSTAMP:19960704T120000Z\n" +
+        "UID:uid1@example.com\n" +
+        "ORGANIZER:mailto:jsmith@example.com\n" +
+        "DTSTART:19960918T143000Z\n" +
+        "DTEND:19960920T220000Z\n" +
+        "STATUS:CONFIRMED\n" +
+        "CATEGORIES:CONFERENCE\n" +
+        "SUMMARY:Networld+Interop Conference\n" +
+        "DESCRIPTION:Networld+Interop Conference and Exhibit\\nAtlanta World Congress Center\\n\n" +
+        "Atlanta\\, Georgia\n" +
+        "END:VEVENT\n" +
+        "END:VCALENDAR";
 
+    const blob = new Blob([data], {type: "text/plain;charset=utf-8"});
+    saveAs(blob, "event.ics");
 }
