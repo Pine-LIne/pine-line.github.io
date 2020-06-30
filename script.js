37d959d4c5d0ec6b8fc870cd0ecf1d5e455c812d
Dre Cardinalli <rcard@hawaii.edu>
2020-06-30 11:39:28 -1000

    Update script.js
    + data now reflects the desired output for check-in..
    
    note:  this was more difficult than anticipated due to the timezone parameters.

--- script.js
+++ script.js
@@ -6,21 +6,30 @@ function submitForm() {
 
 function createFile() {
     const data = "BEGIN:VCALENDAR\n" +
-        "PRODID:-//xyz Corp//NONSGML PDA Calendar Version 1.0//EN\n" +
         "VERSION:2.0\n" +
+        "PRODID:Team-Pine-Line\n" +
+        "CALSCALE:GREGORIAN\n" +
+        "BEGIN:VTIMEZONE\n" +
+        "TZID:Pacific/Honolulu\n" +
+        "TZURL:http://tzurl.org/zoneinfo-outlook/Pacific/Honolulu\n" +
+        "X-LIC-LOCATION:Pacific/Honolulu\n" +
+        "BEGIN:STANDARD\n" +
+        "TZOFFSETFROM:-1000\n" +
+        "TZOFFSETTO:-1000\n" +
+        "TZNAME:HST\n" +
+        "END:STANDARD\n" +
+        "END:VTIMEZONE\n" +
         "BEGIN:VEVENT\n" +
-        "DTSTAMP:19960704T120000Z\n" +
-        "UID:uid1@example.com\n" +
-        "ORGANIZER:mailto:jsmith@example.com\n" +
-        "DTSTART:19960918T143000Z\n" +
-        "DTEND:19960920T220000Z\n" +
-        "STATUS:CONFIRMED\n" +
-        "CATEGORIES:CONFERENCE\n" +
-        "SUMMARY:Networld+Interop Conference\n" +
-        "DESCRIPTION:Networld+Interop Conference and Exhibit\\nAtlanta World Congress Center\\n\n" +
-        "Atlanta\\, Georgia\n" +
+        "DTSTAMP:20200630T000000Z\n" +
+        "UID:\n" +
+        "DTSTART;TZID=Pacific/Honolulu:20200813T100000\n" +
+        "DTEND;TZID=Pacific/Honolulu:20200813T130000\n" +
+        "SUMMARY:Study for Exam\n" +
+        "LOCATION:Hamilton Library\n" +
         "END:VEVENT\n" +
         "END:VCALENDAR";
+
+
 
     const blob = new Blob([data], {type: "text/plain;charset=utf-8"});
     saveAs(blob, "event.ics");
