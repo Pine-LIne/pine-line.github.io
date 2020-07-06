e6b397ff18c474d53211621140bec8d5229ad987
Dre Cardinalli <rcard@hawaii.edu>
2020-07-05 18:12:52 -1000

    updated script.js
    + createFile() data is now partially read from the form fields; summary, location, sent-by, start date, end date are included in this update
    - will need to implement timezone stuff as currently it does not adjust accordingly

--- script.js
+++ script.js
@@ -1,39 +1,31 @@
-//data formatter, taken from: https://codereview.stackexchange.com/questions/184459/getting-the-date-on-yyyymmdd-format
-function yyyymmdd(date) {
+//data formatter, adapted from: https://codereview.stackexchange.com/questions/184459/getting-the-date-on-yyyymmdd-format
+function createDTStamp(date) {
     var x = date;
     var y = x.getFullYear().toString();
     var m = (x.getMonth() + 1).toString();
     var d = x.getDate().toString();
+    var h = x.getHours().toString();
+    var min = x.getMinutes().toString();
+    (min.length == 1) && (min = '0' + min);
+    (h.length == 1) && (h = '0' + h);
     (d.length == 1) && (d = '0' + d);
     (m.length == 1) && (m = '0' + m);
-    var yyyymmdd = y + m + d;
-    return yyyymmdd;
+
+    var DTStamp= y + m + d + "T" + h + min + "00Z";
+    return DTStamp;
 }
 
 function submitForm() {
-    createFile();
+    createFile(createEvent());
 }
 
 function createEvent() {
 
-    const title = document.getElementById('title').value;
-    console.log(title);
-
-    const date = new Date()
-    const DTStamp = yyyymmdd(date) + "T" + date.getHours() + date.getMinutes() + "00";
-    console.log(DTStamp);
-
-    //const startDate = ;
-    //const endDate = ;
-}
-
-function createFile() {
-    /*
-    const data = "BEGIN:VCALENDAR\n" +
+    let string = "BEGIN:VCALENDAR\n" +
         "VERSION:2.0\n" +
         "PRODID:Team-Pine-Line\n" +
         "CALSCALE:GREGORIAN\n" +
-        "BEGIN:VTIMEZONE\n" +
+        /*"BEGIN:VTIMEZONE\n" +
         "TZID:Pacific/Honolulu\n" +
         "TZURL:http://tzurl.org/zoneinfo-outlook/Pacific/Honolulu\n" +
         "X-LIC-LOCATION:Pacific/Honolulu\n" +
@@ -43,18 +35,53 @@ function createFile() {
         "TZNAME:HST\n" +
         "END:STANDARD\n" +
         "END:VTIMEZONE\n" +
-        "BEGIN:VEVENT\n" +
-        "DTSTAMP:20200630T000000Z\n" +
-        "UID:\n" +
-        "DTSTART;TZID=Pacific/Honolulu:20200813T100000\n" +
-        "DTEND;TZID=Pacific/Honolulu:20200813T130000\n" +
-        "SUMMARY:Study for Exam\n" +
-        "LOCATION:Hamilton Library\n" +
-        "END:VEVENT\n" +
-        "END:VCALENDAR";
-     */
+        */"BEGIN:VEVENT";
 
-    createEvent();
+    const summary = document.getElementById('summary').value;
+    console.log(summary);
+
+    string = string.concat("\nSUMMARY:" + summary);
+
+    const location = document.getElementById('location').value;
+    console.log(location);
+
+    string = string.concat("\nLOCATION:" + location);
+
+
+    const sentBy = document.getElementById('sentBy').value;
+    console.log(sentBy);
+
+    string = string.concat("\nSENT-BY:" + sentBy);
+
+    const date = new Date()
+    const DTStamp = createDTStamp(date);
+    console.log(DTStamp);
+    string = string.concat("\nDTSTAMP:" + DTStamp);
+
+    const startDate = document.getElementById('startDate').value.replace(/-/g,'');
+    const startTime = document.getElementById('startTime').value.replace(':','');
+    console.log(startDate);
+    console.log(startTime);
+    const DTStart = startDate + "T" + startTime + "00Z";
+
+    string = string.concat("\nDTSTART:" + DTStart);
+
+    const endDate = document.getElementById('endDate').value.replace(/-/g,'');
+    const endTime = document.getElementById('endTime').value.replace(':','');
+    console.log(endDate);
+    console.log(endTime);
+    const DTEnd = endDate + "T" + endTime + "00Z";
+
+    string = string.concat('\nDTEND:' + DTEnd);
+
+    string = string.concat("\nEND:VEVENT" + "\nEND:VCALENDAR");
+
+    console.log(string);
+
+    return string;
+}
+
+function createFile(data) {
 
     const blob = new Blob([data], {type: "text/plain;charset=utf-8"});
     saveAs(blob, "event.ics");
